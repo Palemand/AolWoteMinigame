325caa75d495f961a297dc78baf72b14aea784d2
Bertram Nellemann Thorsen <202306388@post.au.dk>
2025-08-08 01:23:19 +0200

    Sort of good movement

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ window.addEventListener('load', () => {
     canvas.width = 1280;
     canvas.height = 720;
 
-
-
-
-    class InputHandler {
+    class InputHandler { //handles input for the game
         constructor(game) {
             this.game = game;
+            this.pressedKeys = new Set();
+
             window.addEventListener('keydown', (event) => {
-                this.game.lastKey = 'P' + event.key;
-                console.log('key', this.game.lastKey);
+                this.pressedKeys.add(event.key);
+                console.log('Pressed Key', event.key);
             });
 
             window.addEventListener('keyup', (event) => {
-                this.game.lastKey = 'R' + event.key;
-                console.log('key', this.game.lastKey);
+                this.pressedKeys.delete(event.key);
+                console.log(`Key released: ${event.key}`);
             });
-            console.log("handler created")
+
+            console.log("handler init");
         }
     }
 
@@ -32,14 +32,38 @@ window.addEventListener('load', () => {
             this.x = 150;
             this.y = 150;
             this.speed = 3;
+            this.diagonalSpeed = this.speed / Math.sqrt(2);
 
         }
+
         draw(context) {
             context.fillStyle = 'blue';
             context.fillRect(this.x, this.y, this.width, this.height);
         }
 
-        update() {
+        updatePos() {
+            let keys = this.game.input.pressedKeys;
+            // make better later lol
+            if (keys.has('w') && keys.has('a')) {
+                this.y -= this.diagonalSpeed;
+                this.x -= this.diagonalSpeed;
+            }
+            else if (keys.has('w') && keys.has('d')) {
+                this.y -= this.diagonalSpeed;
+                this.x += this.diagonalSpeed;
+            }
+            else if (keys.has('s') && keys.has('a')) {
+                this.y += this.diagonalSpeed;
+                this.x -= this.diagonalSpeed;
+            }
+            else if (keys.has('s') && keys.has('d')) {
+                this.y += this.diagonalSpeed;
+                this.x += this.diagonalSpeed;
+            }
+            else if (keys.has('w')) this.y -= this.speed;
+            else if (keys.has('s')) this.y += this.speed;
+            else if (keys.has('a')) this.x -= this.speed;
+            else if (keys.has('d')) this.x += this.speed;
 
         }
     }
@@ -58,30 +82,32 @@ window.addEventListener('load', () => {
             context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
+
     class Game {
         constructor(width, height) {
             this.width = width;
             this.height = height;
-            this.lastKey = undefined;
             this.input = new InputHandler(this);
             this.player = new Player(this);
             this.boss = new Boss(this);
         }
         render(context) {
-            this.player.draw(context);
             this.boss.draw(context);
-            this.player.update();
-
+            this.player.draw(context);
         }
     }
 
     let game = new Game(canvas.height, canvas.width)
-
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.player.updatePos();
         game.render(ctx);
         requestAnimationFrame(animate);
     }
     animate();
+
+    //gotta add player movement
+
+
 
 });
